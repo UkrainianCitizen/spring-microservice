27684b79647a0f5c6ff87dcfd8310d836446bcc4
Alexander Huba <ukrainian.citizen@ukr.net>
2018-11-21 20:02:43 +0200

    Fetch @Repository

--- src/main/java/com/spring/microservice/repo/RoleRepository.java
+++ src/main/java/com/spring/microservice/repo/RoleRepository.java
@@ -2,12 +2,14 @@ package com.spring.microservice.repo;
 
 import com.spring.microservice.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * Role repository.
  */
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByRoleName(String name);
 }
--- src/main/java/com/spring/microservice/repo/UserRepository.java
+++ src/main/java/com/spring/microservice/repo/UserRepository.java
@@ -2,14 +2,14 @@ package com.spring.microservice.repo;
 
 import com.spring.microservice.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 /**
  * User repository.
  */
-@RepositoryRestResource
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String userName);
 }
